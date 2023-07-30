6e1d090b5930eda3375bea4ada627adf83d4870a
MartinezBruno <brunomar006@gmail.com>
2023-07-30 17:56:29 -0300

    feat(home): hero
    finish hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,32 +3,35 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section className='relative min-h-[687px]'>
-      <div className='-mt-16 bg-[rgba(11,8,2,0.33)] absolute inset-0'>
+    <section className='relative min-h-[687px] pt-16'>
+      <div className='bg-[rgba(11,8,2,0.33)] absolute inset-0'>
         <Image
           src={hero_img}
           alt='Hero image'
           width={1920}
           height={1080}
-          className='object-cover object-[top_center] w-full h-full relative -z-20'
+          className='object-cover object-[top_0_right_-400px] md:object-[top_center] w-full h-full relative -z-20'
         />
       </div>
       <div className='c-container relative'>
-        <div className='flex flex-col gap-8'>
-          <h1 className='heading1 text-center text-white'>
-            Instant collaboration for remote teams
-          </h1>
-          <p className='paragraph text-white text-center'>
+        <div className='flex flex-col gap-8 max-w-[515px] mx-auto md:mx-0 mt-28'>
+          <h1 className='heading1 text-white'>Instant collaboration for remote teams</h1>
+          <p className='paragraph text-white'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
             nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
-          <form>
+          <form className='flex flex-col md:flex-row gap-6 mt-12'>
             <input
               type='email'
               placeholder='Email'
+              className='py-[13px] px-4 border-[2px] border-[rgba(11,8,2,0.33)] rounded placeholder:text-black focus:outline-none focus:border-blue'
             />
-            <button type='submit'>Get early access</button>
+            <button
+              type='submit'
+              className='text-white text-base font-bold leading-[150%] bg-yellow rounded px-6 py-3'>
+              Get early access
+            </button>
           </form>
         </div>
       </div>
