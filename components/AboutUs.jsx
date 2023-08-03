1fbdf1b06ba45cd5ef14255831aebb7413a79dae
MartinezBruno <brunomar006@gmail.com>
2023-08-03 16:34:51 -0300

    feat(home): About us section

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -1,30 +1,41 @@
 import data from '@/content/about-us.json'
-import Image from 'next/image'
 import Link from 'next/link'
+import Arrow from './Icons/Arrow'
 
 const featuredArticles = data.data
 
 const AboutUs = () => {
   return (
-    <section className='c-container flex flex-col gap-16'>
+    <section
+      className='c-container flex flex-col gap-16 scroll-mt-[104px]'
+      id='about-us'>
       {featuredArticles.map((article, index) => (
         <article
           key={index}
           className={`${
-            index % 2 ? 'flex md:flex-row-reverse self-end' : 'flex md:flex-row'
-          } flex-col max-w-[442px] md:max-w-none`}>
-          <Image
+            index % 2 ? 'flex lg:flex-row-reverse self-end' : 'flex lg:flex-row'
+          } flex-col max-w-[442px] md:max-w-[650px] lg:max-w-none lg:gap-28`}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
             src={article.image}
-            width={242}
-            height={162}
+            alt='Article cover image'
             loading='lazy'
-            className='object-cover w-full h-full'
-            alt='Article Cover image'
+            className='object-cover w-full md:max-w-[55%] h-full'
           />
           <div className='basis-full'>
-            <h2>{article.title}</h2>
-            <p>{article.content.substring(0, 500)}...</p>
-            <Link href={article.handle}>Learn more</Link>
+            <h2 className='text-xl lg:text-5xl font-extrabold text-yellow my-3'>
+              {article.title}
+            </h2>
+            <p className='text-black font-semibold leading-[140%] tracking-[-0.26px] text-sm md:text-base lg:text-xl'>
+              {article.content.substring(0, 300)}...
+            </p>
+            <Link
+              href={article.handle}
+              className={`${
+                index % 2 && 'justify-end'
+              } flex text-sm font-bold leading-[175%] underline mt-5`}>
+              Learn more <Arrow />
+            </Link>
           </div>
         </article>
       ))}
