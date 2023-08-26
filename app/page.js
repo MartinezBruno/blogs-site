805bff3372548af72ef71273693479971f9641a5
MartinezBruno <brunomar006@gmail.com>
2023-08-25 23:07:28 -0300

    feat: add axios

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
+import axios from 'axios'
+
 import AboutUs from '@/components/Home/AboutUs'
 import Hero from '@/components/Home/Hero'
 import RateUs from '@/components/Home/RateUs'
+
+if (process.env.NODE_ENV !== 'production') axios.defaults.baseURL = `${process.env.BASE_URL}/api`
+else axios.defaults.baseURL = `${process.env.BASE_URL}/api`
 
 export default function Home () {
   return (
