ec8c822ac47b3d6f3d11ecd55ab01d4893822134
MartinezBruno <brunomar006@gmail.com>
2023-09-04 02:03:19 -0300

    refactor(blog): post comment
    update alert and error handling

--- app/blogs/[blogId]/PostBlogComment.jsx
+++ app/blogs/[blogId]/PostBlogComment.jsx
@@ -13,7 +13,7 @@ const postComment = async (blogId, comment, userEmail) => {
     if (postComment.status === 200) return true
     throw new Error('Something went wrong')
   } catch (error) {
-    console.log(error)
+    console.error(error)
     return false
   }
 }
@@ -22,7 +22,11 @@ const PostBlogComment = ({ blogId }) => {
   const { data: session } = useSession()
   const [providers, setProviders] = useState(null)
   const [comment, setComment] = useState('')
-  const [message, setMessage] = useState({})
+  const [message, setMessage] = useState({
+    type: '',
+    text: '',
+    alertStyles: ''
+  })
   const limitChars = 300
   const [charsLeft, setCharsLeft] = useState(limitChars)
 
@@ -32,38 +36,52 @@ const PostBlogComment = ({ blogId }) => {
     setCharsLeft(limitChars - e.target.value.length)
   }
 
-  const handleCommentSubmit = (e) => {
+  const handleCommentSubmit = async (e) => {
     e.preventDefault()
-    const postCommentStatus = postComment(blogId, comment, session?.user.email)
+    if (comment.trim().length < 1) {
+      handleMessage('not empty')
+    }
+    const postCommentStatus = await postComment(blogId, comment, session?.user.email)
+    console.log(postCommentStatus)
     if (postCommentStatus) {
       setComment('')
       handleMessage('success')
-    } else {
-      console.log('Something went wrong')
-      handleMessage('error')
+      return
     }
+    handleMessage('error')
   }
 
   const handleMessage = (messageType) => {
-    if (messageType === 'success') {
-      setMessage({
-        type: 'success',
-        text: 'Comment posted successfully'
-      })
-      setTimeout(() => {
-        setMessage(null)
-        window.location.reload()
-      }, 2000)
+    switch (messageType) {
+      case 'success':
+        setMessage({
+          type: 'success',
+          text: 'Comment posted successfully',
+          alertStyles: 'text-green-500'
+        })
+        break
+      case 'error':
+        setMessage({
+          type: 'error',
+          text: 'Something went wrong, try again later',
+          alertStyles: 'text-red-500'
+        })
+        break
+      case 'not empty':
+        setMessage({
+          type: 'warning',
+          text: 'Be careful, your comment cannot be empty',
+          alertStyles: 'text-amber-400'
+        })
+        break
     }
-    if (messageType === 'error') {
+    setTimeout(() => {
       setMessage({
-        type: 'error',
-        text: 'Something went wrong, try again later'
+        ...message,
+        alertStyles: 'max-h-0 '
       })
-      setTimeout(() => {
-        setMessage(null)
-      }, 2000)
-    }
+      if (messageType === 'success') window.location.reload()
+    }, 2000)
   }
 
   useEffect(() => {
@@ -107,7 +125,7 @@ const PostBlogComment = ({ blogId }) => {
             <div className='flex flex-col items-start w-full gap-2'>
               <textarea onChange={handleCommentChange} value={comment} name="comment" id="comment" placeholder='Comment' className='border-[2px] h-[93px] lg:h-[195px] w-full p-4'></textarea>
               <span>{charsLeft} / {limitChars}</span>
-              <p className={message?.type === 'success' ? 'text-green-500' : 'text-red-600'}>{message?.text}</p>
+              <p className={`${message.alertStyles}`}>{message.text}</p>
               <button
                 type='submit'
                 className='text-base mt-2 text-white font-bold leading-[150%] bg-[rgba(255,255,255,0.15)] rounded-[5px] py-2 px-7 bg-yellow hover:opacity-70 transition-[background-color] ease-in-out'>
