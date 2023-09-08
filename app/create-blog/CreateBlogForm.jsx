21f34e78c55d0a2c9ed4863f2c7fb908bdd7b85f
MartinezBruno <brunomar006@gmail.com>
2023-09-08 18:22:41 -0300

    feat(create-blog): add validations and error handlers

--- app/create-blog/CreateBlogForm.jsx
+++ app/create-blog/CreateBlogForm.jsx
@@ -2,18 +2,25 @@
 
 import { uploadImageAndGetURL } from '@/app/services/firebase.config'
 import Spinner from '@/components/Icons/Spinner'
+import SuccessModal from '@/components/SuccessModal'
 import previewImage from '@/public/image-upload-preview.svg'
 import axios from 'axios'
 import { useRef, useState } from 'react'
 
 const postBlog = async (blog, image) => {
-  const res = await axios.post('/api/posts', {
-    title: blog.title,
-    image,
-    content: blog.content,
-    email: blog.email
-  })
-  console.log(res)
+  try {
+    const res = await axios.post('/api/posts', {
+      title: blog.title,
+      image,
+      content: blog.content,
+      email: blog.email
+    })
+    if (res.status !== 200) throw new Error('Something went wrong')
+
+    return res.data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 const CreateBlogForm = ({ user }) => {
@@ -26,7 +33,15 @@ const CreateBlogForm = ({ user }) => {
     content: '',
     email: user.email
   })
+  const [errors, setErrors] = useState([{
+    title: '',
+    banner: '',
+    content: ''
+  }
+  ])
   const [loading, setLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [blogId, setBlogId] = useState('')
 
   const handleImageClick = () => {
     inputRef.current.click()
@@ -46,31 +61,74 @@ const CreateBlogForm = ({ user }) => {
   }
 
   const handleHeight = (e) => {
-    e.target.style.height = 0
-    e.target.style.height = `${e.target.scrollHeight}px`
+    if (e.target.value === '') e.target.style.height = '0px'
+    else e.target.style.height = `${e.target.scrollHeight - 20}px`
+  }
+
+  const handleFocus = (e) => {
+    handleHeight(e)
+    e.target.onblur = () => {
+      e.target.style.height = '0px'
+    }
+  }
+
+  const closeModal = () => {
+    setShowModal(false)
+  }
+
+  const handleErrors = (blog) => {
+    const errors = {}
+    if (!blog.title) errors.title = 'Title is required'
+    if (!blog.banner) errors.banner = 'Banner is required'
+    if (!blog.content) errors.content = 'Content is required'
+
+    setErrors(errors)
+    return Object.keys(errors).length === 0
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     setLoading(true)
-    const imageUrl = await uploadImageAndGetURL({ file: blog.banner, userId: user.id })
-    console.log(imageUrl)
-    setBlog({ ...blog, banner: imageUrl })
-    console.log(blog.banner)
+    const errors = handleErrors(blog)
+    if (!errors) return setLoading(false)
 
-    await postBlog(blog, imageUrl)
+    const imageUrl = await uploadImageAndGetURL({ file: blog.banner, userId: user.id })
+    setBlog({ ...blog, banner: imageUrl })
+
+    const post = await postBlog(blog, imageUrl)
+    if (!post.id) return alert('Something went wrong')
+    postSuccess(post)
+  }
+
+  const postSuccess = (post) => {
+    setBlog({
+      title: '',
+      banner: '',
+      content: '',
+      email: user.email
+    })
+    setPreview(previewImage.src)
     setLoading(false)
+    setShowModal(true)
+    setBlogId(post.id)
   }
 
   return (
     <form className='lg:px-[205px]' onSubmit={handleSubmit}>
       <div className='mb-4 w-full flex flex-col gap-4'>
-        <input type="text" name="title" placeholder="Title" className="border p-3 w-full" onChange={handleOnChange} value={blog.title}/>
+        <div>
+          <input type="text" name="title" placeholder="Title" className="border p-3 w-full" onChange={handleOnChange} value={blog.title}/>
+          {errors.title && <span className='text-red-500'>{errors.title}</span>}
+        </div>
         <div onClick={handleImageClick} className='overflow-hidden'>
           <input type="file" name="image" id="image" ref={inputRef} className='hidden'/>
-          <img src={preview} className='cursor-pointer aspect-video w-full object-cover'/>
+          <img src={preview} className='cursor-pointer aspect-video w-full object-cover object-center' alt='Banner image'/>
+          {errors.banner && <span className='text-red-500'>{errors.banner}</span>}
         </div>
-        <textarea name="content" id="content" placeholder='Type something' className='border p-3 w-full h-fit' onChange={handleOnChange} value={blog.content}></textarea>
+        <div>
+          <textarea name="content" id="content" placeholder='Type something' className='border p-3 w-full min-h-[155px] lg:min-h-[310px] transition-[height] duration-500' onChange={handleOnChange} value={blog.content} onFocus={handleFocus}></textarea>
+          {errors.content && <span className='text-red-500'>{errors.content}</span>}
+        </div>
         <button disabled={loading} type="submit" className='text-base font-bold leading-[150%] rounded-[5px] py-2 px-3 bg-yellow disabled:bg-green-500 transition-[background-color,height] ease-in-out'>
           {loading
             ? (
@@ -82,6 +140,7 @@ const CreateBlogForm = ({ user }) => {
             : 'Post your blog!'}
         </button>
       </div>
+      <SuccessModal showModal={showModal} closeModal={closeModal} blogId={blogId} />
     </form>
   )
 }
