b66f69cbc040f866199092792d08b37c957be66d
MartinezBruno <brunomar006@gmail.com>
2023-09-20 09:47:14 -0300

    update axios base_url

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
-import { BASE_URL } from '@/app/services/config'
-import axios from 'axios'
-
 import Hero from '@/components/Home/Hero'
 
 import AboutUs from '@/components/Home/AboutUs'
 import RateUs from '@/components/Home/RateUs'
-
-if (process.env.NODE_ENV !== 'production') axios.defaults.baseURL = `${BASE_URL}/api`
-else axios.defaults.baseURL = 'http://localhost:3000/api'
 
 export default function Home () {
   return (
