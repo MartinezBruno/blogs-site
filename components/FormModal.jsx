df55397d2e4933f471dcf5a4670a4c5f39e2fce7
MartinezBruno <brunomar006@gmail.com>
2023-09-20 09:44:43 -0300

    feat(profile): profile page banner

--- components/FormModal.jsx
+++ components/FormModal.jsx
@@ -1,15 +1,15 @@
-const FormModal = ({ showModal, closeModal }) => {
+const FormModal = ({ closeModal, handleChange, handleSubmit, userInfo }) => {
   return (
     <div
       id='authentication-modal'
-      aria-hidden='true'
-      className='fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full bg-transparent/25 grid place-content-center'>
+      className='fixed inset-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full bg-transparent/25 flex justify-center items-center'>
+      <div className="fixed inset-0 content-[' ']" onClick={closeModal}></div>
       <div className='relative w-full max-w-md max-h-full'>
-        <div className='relative bg-white rounded-lg shadow dark:bg-gray-700'>
+        <div className='relative bg-white rounded-lg shadow'>
           <button
             type='button'
-            className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white'
-            data-modal-hide='authentication-modal'>
+            className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center'
+            onClick={closeModal}>
             <svg
               className='w-3 h-3'
               aria-hidden='true'
@@ -18,53 +18,71 @@ const FormModal = ({ showModal, closeModal }) => {
               viewBox='0 0 14 14'>
               <path
                 stroke='currentColor'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
               />
             </svg>
             <span className='sr-only'>Close modal</span>
           </button>
           <div className='px-6 py-6 lg:px-8'>
-            <h3 className='mb-4 text-xl font-medium text-gray-900 dark:text-white'>
-              Sign in to our platform
+            <h3 className='mb-4 text-xl font-medium text-gray-900'>
+              Edit your profile
             </h3>
-            <form className='space-y-6' action='#'>
+            <form className='space-y-6' onSubmit={handleSubmit}>
               <div>
                 <label
-                  for='email'
-                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                  Your email
+                  htmlFor='fullname'
+                  className='block mb-2 text-sm font-medium text-gray-900'>
+                  Name
                 </label>
                 <input
-                  type='email'
-                  name='email'
-                  id='email'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-                  placeholder='name@company.com'
-                  required
+                  type='text'
+                  name='fullname'
+                  id='fullname'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+                  autoComplete="off"
+                  value={userInfo.fullname}
+                  onChange={handleChange}
                 />
               </div>
               <div>
                 <label
-                  for='password'
-                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                  Your password
+                  htmlFor='biography'
+                  className='block mb-2 text-sm font-medium text-gray-900'>
+                  Bio
                 </label>
                 <input
-                  type='password'
-                  name='password'
-                  id='password'
-                  placeholder='••••••••'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-                  required
+                  type='text'
+                  name='bio'
+                  id='biography'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+                  autoComplete="off"
+                  value={userInfo.bio}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor='position'
+                  className='block mb-2 text-sm font-medium text-gray-900'>
+                  Position
+                </label>
+                <input
+                  type='text'
+                  name='position'
+                  id='position'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+                  autoComplete="off"
+                  value={userInfo.position}
+                  onChange={handleChange}
                 />
               </div>
               <button
                 type='submit'
-                className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
-                Login to your account
+                className='w-full text-white bg-yellow hover:brightness-75 transition-all ease-in-out focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>
+                Edit your profile info
               </button>
             </form>
           </div>
