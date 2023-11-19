870520c4b6c9dcf57b622dba2bb942f072c05291
Bruno Martinez <brunomar006@gmail.com>
2023-11-19 00:04:54 -0300

    feat: blog card
    add Edit and Delete options for blog owner

--- components/Blogs/BlogCard.jsx
+++ components/Blogs/BlogCard.jsx
@@ -1,13 +1,19 @@
+import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import HorizontalDots from '../Icons/HorizontalDots'
 import styles from './BlogCard.module.css'
 
-const BlogCard = ({ blog }) => {
+const BlogCard = ({ blog, userId }) => {
+  const { data: session } = useSession()
+
   return (
-    <Link href={`/blogs/${blog.id}`}>
-      <article
-        className='rounded-lg shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'>
-        {blog.banner && (
+    <article className='rounded-lg shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'>
+      {session?.user?.id === userId && (
+        <HorizontalDots blogId={blog.id}/>
+      )}
+      {blog.banner && (
+        <Link href={`/blogs/${blog.id}`}>
           <div className='overflow-hidden rounded-lg flex-[1_0_100%] flex'>
             <Image
               src={blog.banner}
@@ -18,25 +24,31 @@ const BlogCard = ({ blog }) => {
               className='w-full h-full object-cover object-center blog-img aspect-square'
             />
           </div>
-        )}
-        <div className='p-5'>
-          <h2 className='text-xl lg:text-2xl h-[2.6em] truncate whitespace-normal sm:whitespace-nowrap mb-2'>{blog.title}</h2>
-          <p className={styles.blog_content}>{blog.content}</p>
-          <div className='flex justify-start items-center gap-2 mt-11'>
-            <img
-              src={blog.authorImage ?? 'https://via.placeholder.com/150'}
-              width={40}
-              height={40}
-              alt='user photo'
-              className='rounded-full'
-            />
-            <span className='text-text_gray text-sm font-semibold'>
-              {blog.authorName ?? 'Unknown author'} <span className=' hidden md:inline-block'> | {new Date(blog.createdAt).toDateString()}</span>
+        </Link>
+      )}
+      <div className='p-5'>
+        <h2 className='text-xl lg:text-2xl h-[2.6em] truncate whitespace-normal sm:whitespace-nowrap mb-2'>
+          {blog.title}
+        </h2>
+        <p className={styles.blog_content}>{blog.content}</p>
+        <div className='flex justify-start items-center gap-2 mt-11'>
+          <img
+            src={blog.authorImage ?? 'https://via.placeholder.com/150'}
+            width={40}
+            height={40}
+            alt='user photo'
+            className='rounded-full'
+          />
+          <span className='text-text_gray text-sm font-semibold'>
+            {blog.authorName ?? 'Unknown author'}{' '}
+            <span className=' hidden md:inline-block'>
+              {' '}
+              | {new Date(blog.createdAt).toDateString()}
             </span>
-          </div>
+          </span>
         </div>
-      </article>
-    </Link>
+      </div>
+    </article>
   )
 }
 
---
+++ components/Icons/HorizontalDots.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+const HorizontalDots = ({ blogId }) => {
+  const router = useRouter()
+  const [showMenu, setShowMenu] = useState(false)
+  const toggleMenu = () => setShowMenu(!showMenu)
+  const handleDelete = async () => {
+    await axios.delete(`/api/posts/${blogId}`)
+    router.replace()
+  }
+  return (
+    <div className='absolute right-0'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='40'
+        height='40'
+        viewBox='0 0 40 40'
+        fill='none'
+        className='w-14 h-auto'
+        onClick={toggleMenu}>
+        <path
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M8.49984 14.1667C6.9415 14.1667 5.6665 15.4417 5.6665 17C5.6665 18.5583 6.9415 19.8333 8.49984 19.8333C10.0582 19.8333 11.3332 18.5583 11.3332 17C11.3332 15.4417 10.0582 14.1667 8.49984 14.1667ZM25.4998 14.1667C23.9415 14.1667 22.6665 15.4417 22.6665 17C22.6665 18.5583 23.9415 19.8333 25.4998 19.8333C27.0582 19.8333 28.3332 18.5583 28.3332 17C28.3332 15.4417 27.0582 14.1667 25.4998 14.1667ZM14.1665 17C14.1665 15.4417 15.4415 14.1667 16.9998 14.1667C18.5582 14.1667 19.8332 15.4417 19.8332 17C19.8332 18.5583 18.5582 19.8333 16.9998 19.8333C15.4415 19.8333 14.1665 18.5583 14.1665 17Z'
+          fill='#FFFFFF'
+          stroke='#000000'
+        />
+      </svg>
+      {showMenu && (
+        <div className='absolute right-6 top-9 bg-white flex flex-col'>
+          <span className='dropdown_link cursor-pointer'>Edit</span>
+          <span className='dropdown_link cursor-pointer' onClick={handleDelete}>Delete</span>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default HorizontalDots
