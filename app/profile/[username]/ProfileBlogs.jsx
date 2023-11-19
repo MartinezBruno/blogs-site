c76a3bd523d3283f0af6c631e848b73fca9be497
Bruno Martinez <brunomar006@gmail.com>
2023-11-19 00:03:53 -0300

    feat: add user data request
    optimize two calls into a Promise.all()

--- app/profile/[username]/ProfileBlogs.jsx
+++ app/profile/[username]/ProfileBlogs.jsx
@@ -5,16 +5,33 @@ const getUserBlogs = async (username) => {
   const res = await fetch(`${BASE_URL}/api/posts/user/${username}`, {
     next: { revalidate: 300 }
   })
-  const data = await res.json()
-  return data
+  const userBlogs = await res.json()
+  return userBlogs
+}
+const getUserInfo = async (username) => {
+  const res = await fetch(`${BASE_URL}/api/user/${username}`, {
+    cache: 'no-cache'
+  })
+  const userData = await res.json()
+  return userData
 }
 
 const ProfileBlogs = async ({ username }) => {
-  const userBlogs = await getUserBlogs(username)
+  let userBlogs = []
+  let userData = []
+  try {
+    [userBlogs, userData] = await Promise.all([
+      getUserBlogs(username),
+      getUserInfo(username)
+    ])
+  } catch (error) {
+    console.error(error)
+  }
+
   return (
     <div className='mt-3 md:mt-10'>
       <h3 className='font-bold text-xl md:text-3xl lg:text-[40px] mb-3'>Posts</h3>
-      <ProfileBlogsGrid blogs={userBlogs} />
+      <ProfileBlogsGrid blogs={userBlogs} user={userData} />
     </div>
   )
 }
